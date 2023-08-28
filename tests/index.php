11d6f10dc9ac0c5f578af33e7c4b359e2b328e96
yoshi-gid <yoshipanman1201@gmail.com>
2023-08-28 17:58:15 +0900

    テスト一覧ページの作成

--- tests/index.php
+++ tests/index.php
@@ -1,3 +1,10 @@
+<?php
+session_start();
+require('../db/dbconnect.php');
+
+$tests = $db->query('SELECT * from tests');
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,13 +15,17 @@
 <body>
     <h1>テスト一覧</h1>
     <table border=1>
-        <tr><th>番号</th><th>学年</th><th>テスト名</th><th>作成日</th></tr>
+        <tr><th>番号</th><th>学年</th><th>テスト名</th><th>作成日</th><th>変更はこちら</th><th>削除するか</th></tr>
+        <?php foreach($tests as $test): ?>
         <tr>
-            <td>1</td>
-            <td>2</td>
-            <td>中間テスト</td>
-            <td>2022-11-11</td>
+            <td><?php echo $test['id']; ?></td>
+            <td><?php echo $test['year']; ?></td>
+            <td><?php echo $test['name']; ?></td>
+            <td><?php echo $test['create_at']; ?></td>
+            <td><a href="edit.php?id=<?php echo $test['id']; ?>">変更</a></td>
+            <td><a href="destory.php?id=<?php echo $test['id']; ?>">削除</a></td>
         </tr>
+        <?php endforeach; ?>
     </table>
     <a href="create.php">テストを作成する</a>
 </body>
