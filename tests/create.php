efd67effa0215cc73c6773d44de13ac9b6c08e1c
yoshi-gid <yoshipanman1201@gmail.com>
2023-08-28 15:57:23 +0900

    Update create.php
    テストを登録するための、ファイルを作成

---
+++ tests/create.php
@@ -0,0 +1,63 @@
+<?php
+session_start();
+require('../db/dbconnect.php');
+
+if(!empty($_POST)){
+    $_SESSION['join'] = $_POST;
+
+    if(preg_match( '/^[0-9]+$/', $_POST['year']) == 0){
+        $error['year'] = 'hankaku';
+    }
+
+    if($_POST['year'] == ''){
+        $error['year'] = 'blank';
+    }
+
+    if($_POST['name'] == ''){
+        $error['name'] = 'blank';
+    }
+
+    if(empty($error)){
+        $stmt = $db->prepare('INSERT into tests set year=?, name=?, create_at=NOW()');
+        echo $ret = $stmt->execute(array(
+            $_SESSION['join']['year'],
+            $_SESSION['join']['name']
+        ));
+    }
+    
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>成績管理アプリ</title>
+</head>
+<body>
+    <form method="post" action="">
+
+        <label for="year">学年（1〜３の数字で入力）</label><br>
+        <input type="text" name="year" id="year">
+        <?php if($error['year'] == 'hankaku'): ?>
+            <p style="color:red;">半角数字で入力してください</p>
+        <?php endif; ?>
+
+        <?php if($error['year'] == 'blank'): ?>
+            <p style="color:red;">入力してください</p>
+        <?php endif; ?>
+        <br><br>
+
+        <label for="name">テスト名</label><br>
+        <input type="text" name="name" id="name">
+        <?php if($error['name'] == 'blank'): ?>
+            <p style="color:red;">入力してください</p>
+        <?php endif; ?>
+        <br><br>
+
+        <input type="submit" value="追加する">
+    </form>
+</body>
+</html>
