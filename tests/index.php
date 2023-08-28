745b9930344aed9617860bfdfd1e1da9171756f8
yoshi-gid <yoshipanman1201@gmail.com>
2023-08-28 15:57:43 +0900

    Update index.php
    成績を表示するためのファイルを作成

---
+++ tests/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>成績管理アプリ</title>
+</head>
+<body>
+    <h1>テスト一覧</h1>
+    <table border=1>
+        <tr><th>番号</th><th>学年</th><th>テスト名</th><th>作成日</th></tr>
+        <tr>
+            <td>1</td>
+            <td>2</td>
+            <td>中間テスト</td>
+            <td>2022-11-11</td>
+        </tr>
+    </table>
+    <a href="create.php">テストを作成する</a>
+</body>
+</html>
