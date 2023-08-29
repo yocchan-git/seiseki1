d7c5c530d00ebf4538a24e8bdeb9cc872143c451
yoshi-gid <yoshipanman1201@gmail.com>
2023-08-29 20:14:53 +0900

    データベース名を変更

--- db/dbconnect.php
+++ db/dbconnect.php
@@ -1,6 +1,6 @@
 <?php
 try{
-    $db = new PDO('mysql:dbname=seiseki1;host=localhost;charset=utf8','root','root');
+    $db = new PDO('mysql:dbname=seiseki;host=localhost;charset=utf8','root','root');
 }catch(PDOException $e){
     echo 'DB接続エラー:'.$e->getMessage();
 }
