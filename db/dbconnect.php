e4ec0d61d33a059ce0c5a1a58af42a1d5694ac02
yoshi-gid <yoshipanman1201@gmail.com>
2023-09-11 21:47:48 +0900

    データベース接続（クラスなし）処理

--- db/dbconnect.php
+++ db/dbconnect.php
@@ -1,6 +1,6 @@
 <?php
 try{
-    $db = new PDO('mysql:dbname=seiseki;host=localhost;charset=utf8','root','root');
+    $db = new PDO('mysql:dbname=rkmcl_school;host=mysql93.conoha.ne.jp;charset=utf8mb4','rkmcl_school','school&2525');
 }catch(PDOException $e){
     echo 'DB接続エラー:'.$e->getMessage();
 }
