12ac8253a9aa161b59b293fcd1e4e049d4b30bc6
yoshi-gid <yoshipanman1201@gmail.com>
2023-09-11 21:48:51 +0900

    ダウンロードする画面

--- exams/download.php
+++ exams/download.php
@@ -1,69 +1,9 @@
-<!-- <?php
-session_start();
-require('../db/dbconnect.php');
-
-$test_id = $_GET['id'];
-$rank = $_GET['rank'];
-
-$sql = 'SELECT st.number,st.name as student_name,te.name as test_name,ex.kokugo,ex.sugaku,ex.eigo,ex.rika,ex.shakai,ex.goukei
-from exams as ex
-inner join tests as te
-on ex.test_id = te.id
-    inner join students as st
-    on ex.student_id = st.id
-where ex.test_id=?';
-
-$stmt = $db->prepare($sql);
-$stmt->execute(array($test_id));
-
-$csv = '学籍番号,生徒名,国語,数学,英語,理科,社会,合計';
-$csv .= "\n";
-
-while(true){
-    $rec = $stmt->fetch(PDO::FETCH_ASSOC);
-    if($rec == false){
-        break;
-    }
-    $csv .= $rec['number'];
-    $csv .= ',';
-    $csv .= $rec['student_name'];
-    $csv .= ',';
-    $csv .= $rec['kokugo'];
-    $csv .= ',';
-    $csv .= $rec['sugaku'];
-    $csv .= ',';
-    $csv .= $rec['eigo'];
-    $csv .= ',';
-    $csv .= $rec['rika'];
-    $csv .= ',';
-    $csv .= $rec['shakai'];
-    $csv .= ',';
-    $csv .= $rec['goukei'];
-    $csv .= "\n";
-}
-
-// echo nl2br($csv);
-
-$file = fopen('./exam.csv','w');
-fputs($file,$csv);
-fclose($file);
-
-?> -->
-
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>成績管理アプリ</title>
-</head>
-<body>
 <?php
-session_start();
-require('../db/dbconnect.php');
+require('../auth/login-check.php');
+require('../components/header.php');
 
 $test_id = $_GET['id'];
-$rank = $_GET['rank'];
+$year = $_GET['year'];
 
 $sql = 'SELECT st.number,st.name as student_name,te.name as test_name,ex.kokugo,ex.sugaku,ex.eigo,ex.rika,ex.shakai,ex.goukei
 from exams as ex
@@ -109,8 +49,14 @@ fputs($file,$csv);
 fclose($file);
 
 ?>
+<div class="container">
+    <div class="mt-3 mb-3">
+        <p>ダウンロードできるのは選んだテストの結果だけです（ランキングではない）</p>
+    </div>
+    <a href="exam.csv">成績のダウンロードはこちら</a><br><br>
+    <a class="btn btn-secondary" href="ranking.php?id=<?php echo $test_id; ?>&year=<?php echo $year; ?>">戻る</a>
+</div>
 
-<a href="exam.csv">成績のダウンロードはこちら</a><br><br>
-<a href="ranking.php?id=<?php echo $test_id; ?>&rank=<?php echo $rank; ?>">戻る</a>
-</body>
-</html>
+<?php
+require('../components/footer.php');
+?>
