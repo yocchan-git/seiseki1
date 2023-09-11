85835661afdfceda457e483f09a0f55cb2c96412
yoshi-gid <yoshipanman1201@gmail.com>
2023-09-11 21:46:05 +0900

    新規登録後の画面を作成

---
+++ auth/complete.php
@@ -0,0 +1,13 @@
+<?php
+require('../components/header.php');
+?>
+<!-- 先生登録が完了しましたを伝える -->
+<div class="text-center mt-3">
+    <p>先生を登録しました</p>
+    <!-- ログイン画面に移動させる -->
+    <a class="btn btn-primary" href="login.php">ログインする</a>
+</div>
+
+<?php
+require('../components/footer.php');
+?>
