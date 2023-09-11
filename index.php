9d56d28811cd5153bce9a9d178937c45120b1f49
yoshi-gid <yoshipanman1201@gmail.com>
2023-09-11 21:50:42 +0900

    エラー用にindex.phpを作成

--- index.php
+++ index.php
@@ -1,14 +1,4 @@
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>成績管理アプリ</title>
-</head>
-<body>
-    <h1>トップページ</h1><br><br>
-    <a href="students/index.php">生徒一覧ページ</a><br><br>
-    <a href="tests/index.php">テストページ</a><br><br>
-    <a href="exams/index.php">テスト成績ページ</a><br><br>
-</body>
-</html>
+<?php
+header('Location:teachers/index.php');
+exit();
+?>
