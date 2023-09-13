b99962d1432ea1956c01effd309c7cd0d4b172c3
yoshi-gid <yoshipanman1201@gmail.com>
2023-09-13 18:34:11 +0900

    dbconnect.phpを削除

--- db/dbconnect.php
+++
@@ -1,7 +0,0 @@
-<?php
-try{
-    $db = new PDO('mysql:dbname=rkmcl_school;host=mysql93.conoha.ne.jp;charset=utf8mb4','rkmcl_school','school&2525');
-}catch(PDOException $e){
-    echo 'DB接続エラー:'.$e->getMessage();
-}
-?>
