6bdd88486cc16232d6d56f02517f60286a35d499
yoshi-gid <yoshipanman1201@gmail.com>
2023-09-13 18:38:57 +0900

    トップページも権限によって変わる

--- teachers/index.php
+++ teachers/index.php
@@ -1,6 +1,13 @@
 <?php
 require('../auth/login-check.php');
 require('../components/header.php');
+
+if($_SESSION['post'] == 2){
+    if(empty($_SESSION['shuninYear'])){
+        header('Location:shunin-year.php');
+        exit();
+    }
+}
 ?>
     <h1>トップページ</h1><br><br>
 
@@ -25,9 +32,31 @@ require('../components/header.php');
 
         <div class="col-6">
             <div class="card p-3">
+                <a href="../classes/index.php">クラス一覧</a>
+            </div>
+        </div>
+
+        <div class="col-6">
+            <div class="card p-3">
                 <a href="../auth/logout.php">ログアウト</a>
             </div>
         </div>
+
+        <?php if($_SESSION['post'] == 1): ?>
+            <div class="col-6">
+                <div class="card p-3">
+                    <a href="../teacher_classes/index.php">担当学年を変える</a>
+                </div>
+            </div>
+        <?php endif; ?>
+
+        <?php if($_SESSION['post'] == 2): ?>
+            <div class="col-6">
+                <div class="card p-3">
+                    <a href="shunin-year.php">担当学年を変える</a>
+                </div>
+            </div>
+        <?php endif; ?>
     </div>
 
 <?php
